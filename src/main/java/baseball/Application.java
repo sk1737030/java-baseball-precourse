d7ec55adb00237fbfe4e76f5db2f62f9e76d5979
sk1737030 <sk1737030@naver.com>
2021-05-08 15:41:27 +0900

    feat(baseball): 기능 완성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,8 +1,36 @@
 package baseball;
 
+import baseball.domain.NumberGenerator;
+import baseball.domain.Referee;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
 public class Application {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World");
+        NumberGenerator generator = new NumberGenerator();
+        List<Integer> computer = generator.createRandomNumbers();
+
+        Referee referee = new Referee();
+
+        String result = "";
+        while (!result.equals("0 볼 3 스트라이크")) {
+            result = referee.compare(computer, askNumbers());
+            System.out.println(result);
+        }
+
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    }
+
+    public static List<Integer> askNumbers() {
+        System.out.println("숫자를 입력해 주세요: ");
+        Scanner scanner = new Scanner(System.in);
+        String player = scanner.next();
+        return Arrays.stream(player.split(""))
+                .map(Integer::valueOf)
+                .collect(Collectors.toList());
     }
 }
