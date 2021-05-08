da58438134fe492d859f2ccd42c684a7534fccef
sk1737030 <sk1737030@naver.com>
2021-05-08 16:05:34 +0900

    refactor(baseball): 테스트 중복 제거

--- src/test/java/baseball/domain/RefereeTest.java
+++ src/test/java/baseball/domain/RefereeTest.java
@@ -1,13 +1,13 @@
 package baseball.domain;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class RefereeTest {
     private Referee referee;
@@ -18,37 +18,10 @@ class RefereeTest {
         referee = new Referee();
     }
 
-
-    @Test
-    void 스트라이크3() throws Exception {
-        // Given
-        List<Integer> players = Arrays.asList(1, 2, 3);
-
-        // When
-        String result = referee.compare(ANSWER, players);
-        // Then
-        assertNotEquals("0 볼 2 스트라이크", result);
-        assertEquals("0 볼 3 스트라이크", result);
-    }
-
-    @Test
-    void 볼() throws Exception {
-        // Given
-        List<Integer> players = Arrays.asList(2, 3, 1);
-        // When
-        String result = referee.compare(ANSWER, players);
-        // Then
-        assertEquals("3 볼 0 스트라이크", result);
-    }
-
-    @Test
-    void 아웃() throws Exception {
-        // Given
-        List<Integer> players = Arrays.asList(1, 2, 3);
-        // When
-
-        String result = referee.compare(ANSWER, players);
-        // Then
-        assertEquals("아웃", result);
+    @ParameterizedTest
+    @CsvSource({"1,2,3,0 볼 3 스트라이크", "7, 8, 9,아웃", "2,3,1,3 볼 0 스트라이크", "1,3,2,2 볼 1 스트라이크"})
+    void compare(int number1, int number2, int number3, String expected) {
+        String result = referee.compare(ANSWER, Arrays.asList(number1, number2, number3));
+        assertThat(result).isEqualTo(expected);
     }
 }
